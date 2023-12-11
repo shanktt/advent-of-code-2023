aad79b3219c69e0c67db413e1e760c3bedec3f90
AshankKumar <ashankkumar01@gmail.com>
2023-12-11 11:39:41 -0500

    simplifcations

--- py/days/day11.py
+++ py/days/day11.py
@@ -1,4 +1,3 @@
-from itertools import combinations
 from bisect import bisect_left,bisect_right
 
 def get_answer(grid,expansion_size=1):
@@ -15,21 +14,24 @@ def get_answer(grid,expansion_size=1):
         if all([grid[r][c] == "." for r in range(N)]):
             expanded_cols.append(c)
 
-    galaxy_cords = []
+    galaxies = []
     for r in range(N):
         for c in range(M):
             if grid[r][c] == "#":
-                galaxy_cords.append((r,c))
-    unique_pairs = list(combinations(galaxy_cords, 2))
+                galaxies.append((r,c))
+   
     total = 0
-    for s,t in unique_pairs:
-        start_r,end_r = min(s[0],t[0]),max(s[0],t[0])
-        start_c,end_c = min(s[1],t[1]),max(s[1],t[1])
+    for i in range(len(galaxies)):
+        for j in range(i,len(galaxies)):
+            s = galaxies[i]
+            t = galaxies[j]
+            start_r,end_r = min(s[0],t[0]),max(s[0],t[0])
+            start_c,end_c = min(s[1],t[1]),max(s[1],t[1])
 
-        a = bisect_right(expanded_rows,end_r) -  bisect_left(expanded_rows,start_r) 
-        b = bisect_right(expanded_cols,end_c) - bisect_left(expanded_cols,start_c)
+            a = bisect_right(expanded_rows,end_r) -  bisect_left(expanded_rows,start_r) 
+            b = bisect_right(expanded_cols,end_c) - bisect_left(expanded_cols,start_c)
 
-        total += abs(s[0]-t[0]) + abs(s[1]-t[1]) + (a*expansion_size) + (b*expansion_size)
+            total += abs(s[0]-t[0]) + abs(s[1]-t[1]) + (a*expansion_size) + (b*expansion_size)
 
     return total
 
