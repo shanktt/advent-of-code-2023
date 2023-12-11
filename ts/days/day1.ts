889666fb8d08f8ff4b4ed23215e3b65db1e30550
AshankKumar <ashankkumar01@gmail.com>
2023-12-11 12:15:45 -0500

    ooh thats nice

--- ts/days/day1.ts
+++ ts/days/day1.ts
@@ -27,24 +27,17 @@ export function part2(inputData: string): number {
         "nine": 9,
     };
 
-
-    let sum = 0
-    lines.forEach((line) => {
+    return lines.reduce((sum,line) => {
         let cur_digits: number[] = [];
         for (let i = 0; i < line.length; i++) {
-            Object.keys(strToInt)
-                .map((str) => [str,strToInt[str]] as [string,number])
-                .forEach((digit_int) => {
-                    const digit = digit_int[0]
-                    if (line.substring(i,i+digit.length) == digit) {
-                        cur_digits.push(Number(strToInt[line.substring(i,i+digit.length)]))
-                    } else if (line[i] >= "0" && line[i] <= "9") {
-                        cur_digits.push(Number(line[i]))
-                    }
-                })
+            Object.entries(strToInt).forEach(([str,int]) => {
+                if (line.substring(i,i+str.length) == str) {
+                    cur_digits.push(int)
+                } else if (line[i] >= "0" && line[i] <= "9") {
+                    cur_digits.push(Number(line[i]))
+                }
+            });
         };
-        sum += ((cur_digits[0]*10) + cur_digits[cur_digits.length - 1]);
-    });
-
-    return sum;
+        return sum+((cur_digits[0]*10) + cur_digits[cur_digits.length - 1]);
+    },0);
 }
