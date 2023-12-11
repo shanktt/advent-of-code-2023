eb460f2ee8216a8967a18facb855ed4c10086e19
AshankKumar <ashankkumar01@gmail.com>
2023-12-11 12:05:21 -0500

    ewww... im bad

--- ts/days/day1.ts
+++ ts/days/day1.ts
@@ -13,20 +13,9 @@ export function part1(inputData: string): number {
 }
 
 export function part2(inputData: string): number {
-    // const strToInt = {
-    //     "one": 1,
-    //     "two": 2,
-    //     "three": 3,
-    //     "four": 4,
-    //     "five": 5,
-    //     "six": 6,
-    //     "seven": 7,
-    //     "eight": 8,
-    //     "nine": 9,
-    // }
     const lines = inputData.split('\n')
 
-    const strToInt = {
+    const strToInt: { [key: string]: number } = {
         "one": 1,
         "two": 2,
         "three": 3,
@@ -38,18 +27,23 @@ export function part2(inputData: string): number {
         "nine": 9,
     };
 
+
     let sum = 0
     lines.forEach((line) => {
-        let cur_digits = [];
+        let cur_digits: number[] = [];
         for (let i = 0; i < line.length; i++) {
-            Object.entries(strToInt).forEach((digit,int) => {
-                const len_digit = digit.length;
-
-                if (digit.substring(i, i+len_digit) === digit) {
-                    cur_digits.push()
-                }
-            });
-        }
+            Object.keys(strToInt)
+                .map((str) => [str,strToInt[str]] as [string,number])
+                .forEach((digit_int) => {
+                    const digit = digit_int[0]
+                    if (line.substring(i,i+digit.length) == digit) {
+                        cur_digits.push(Number(strToInt[line.substring(i,i+digit.length)]))
+                    } else if (line[i] >= "0" && line[i] <= "9") {
+                        cur_digits.push(Number(line[i]))
+                    }
+                })
+        };
+        sum += ((cur_digits[0]*10) + cur_digits[cur_digits.length - 1]);
     });
 
     return sum;
